639731cc350eeee8519370d46a5554e6f9e01896
pashtet482 <dydaevmakx41@gmail.com>
2025-05-17 02:14:02 +0300

    подняли сервер, свагер работает

---
+++ src/main/java/com/DocByte/e_shop/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.DocByte.e_shop.controller;
+
+import com.DocByte.e_shop.model.User;
+import com.DocByte.e_shop.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
---
+++ src/main/java/com/DocByte/e_shop/model/User.java
@@ -0,0 +1,25 @@
+package com.DocByte.e_shop.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+    private Boolean role;
+    private final LocalDateTime createdAt = LocalDateTime.now();
+}
+
